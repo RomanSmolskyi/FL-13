5718664aef4a7483b455b64e0c55ec8598c1a999
Roman Smolskyi <55927044+RomanSmolskyi@users.noreply.github.com>
2020-05-10 23:15:16 +0300

    Add files via upload

---
+++ FL13_HW10/homework/js/fighters_game.js
@@ -0,0 +1,10 @@
+class Fighter {
+  constructor(element) {
+    const name = element.name;
+    const damage = element.damage;
+    this.getName = () => name;
+    this.getDamage = () => damage;
+  }
+}
+
+//Sorry, I can't understand how make this task
