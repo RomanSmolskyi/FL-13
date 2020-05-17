8457ab92a39e054c71bf62ca04e95b1f00c7996d
Roman Smolskyi <55927044+RomanSmolskyi@users.noreply.github.com>
2020-05-17 21:12:40 +0300

    Add files via upload

---
+++ FL13_HW12/homework/books.js
@@ -0,0 +1,33 @@
+let booksData = [
+  {
+    id: `1`,
+    title: `Harry Potter`,
+    author: `Joanne Rowling`,
+    image: `harry-potter.jpg`,
+    plot: `An eleven year old boy, Harry Potter, who lives with his uncle, aunt and cousin,
+    having lost his parents as an infant, finds out that he's a wizard (someone with magical powers) 
+    and attends Hogwarts School of Witchcraft and Wizardry. There he makes many friends, 
+    the best of them being Ron Weasley and Hermione Granger.`
+  },
+  {
+    id: `2`,
+    title: `The Outsider`,
+    author: `Stephen Edwin King`,
+    image: `outsider.jpg`,
+    plot: `The novel tells the story of Ponyboy Curtis and his struggles with right and wrong in a society in which
+     he believes that he is an outsider. 
+    Ponyboy and his two brothers — Darrel (Darry), who is 20, and Sodapop, who is 16 — have recently lost their parents
+     in an automobile accident.`
+  },
+  {
+    id: `3`,
+    title: `Eat That Frog`,
+    author: `Brian Tracy`,
+    image: `EatThatFrog.jpg`,
+    plot: `Eat that frog!” means to start your day with the biggest, most important, and most dreaded task.
+     It's also the task you are most likely to procrastinate on.
+    If you do, you will reach new heights of productivity while learning to overcome procrastination 
+    and while building self-discipline.`
+  }
+];
+
